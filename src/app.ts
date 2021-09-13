ed8e71220fd191b142c2a362b8accc2573ec3142
Andre Silva <andre@particle41.com>
2021-09-13 00:02:57 -0300

    init

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,31 @@
-async function handler() {
-  //
+import { APIGatewayEvent } from 'aws-lambda';
+
+type Event = APIGatewayEvent;
+
+async function handler(event: Event) {
+  const {
+    requestContext: { connectionId, routeKey },
+  } = event;
+
+  if (routeKey === '$connect') {
+    // handle new connection
+    console.log(connectionId);
+    return {
+      statusCode: 200,
+    };
+  }
+
+  if (routeKey === '$disconnect') {
+    // handle disconnection
+    return {
+      statusCode: 200,
+    };
+  }
+
+  // $default handler
+  return {
+    statusCode: 200,
+  };
 }
 
 export { handler };
---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+/*
+
+const AWS = require("aws-sdk");
+
+const apig = new AWS.ApiGatewayManagementApi({
+  endpoint: process.env.APIG_ENDPOINT,
+});
+
+    await apig
+      .postToConnection({
+        ConnectionId: connectionId,
+        Data: JSON.stringify(body),
+      })
+      .promise();
+
+// https://<api-id>.execute-api.<region>.amazonaws.com/<stage>
+
+// https://tsh.io/blog/implementing-websocket-with-aws-lambda-and-api-gateway/
+*/
